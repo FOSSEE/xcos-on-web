f9b8c8d4944e0a67b86221a282026949f791c6e9
AMIT KUMAR YADAV <amit.2424.yadav@gmail.com>
2016-06-07 08:04:08 +0530

    Blocks SUPER_f ,INIMPL_f ,IN_f ,OUTIMPL_f ,OUT_f ,CLKINV_f and CLKOUTV_f must be handled separately

--- blocks_xcos/Checker.py
+++ blocks_xcos/Checker.py
@@ -1,8 +1,16 @@
 import os
 
+# Blocks SUPER_f ,INIMPL_f ,IN_f ,OUTIMPL_f ,OUT_f ,CLKINV_f and CLKOUTV_f don't contain simulationFunctionName attribute as their name 
+# hence these blocks must be handled separately
+
+
+
 nameList=os.listdir("xcos")
+script_dir = os.path.dirname(__file__)
+rel_path = "xcos"
+abs_file_path = os.path.join(script_dir, rel_path)
 for i in nameList:
-    ob=open(i,"r")
+    ob=open(os.path.join(abs_file_path,i),"r")
     name=i[:-5]
     fileVal=ob.readline()
 
