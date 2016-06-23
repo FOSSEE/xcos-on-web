271f6c40b3b466be1b467d3b27bf9e7b4cc91cc4
ASP1234 <nimish@nimishs.in>
2016-06-23 12:44:07 +0000

    Implement CLR Block

---
+++ data_structures_correct/CLR.js
@@ -0,0 +1,27 @@
+function CLR () {
+    
+    CLR.prototype.define = function CLR() {
+    	this.x0 = 0;
+    	this.A = -1;
+    	this.B = 1;
+    	this.C = 1;
+    	this.D = 0;
+    
+    	var model = scicos_model();
+    	model.sim=list(new ScilabString(["csslti4"]),new ScilabDouble([4]));
+    	model.in = new ScilabDouble([1]);
+    	model.out = new ScilabDouble([1]);
+    	model.state=new ScilabDouble([this.x0]);
+    	model.rpar = new ScilabDouble([this.A],[this.B],[this.C],[this.D]);
+    	model.blocktype = new ScilabString(["c"]);
+    	model.dep_ut = new ScilabBoolean([false,true]);
+    
+    	var exprs = new ScilabString(["1"],["1+s"]);
+    	var gr_i = [];
+    	this.x=new standard_define(new ScilabDouble([3,2]),model,exprs,gr_i);
+    	return new BasicBlock(this.x);
+    }
+    CLR.prototype.details = function CLR() {
+        return this.x;
+    }
+}
