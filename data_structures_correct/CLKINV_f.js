52bab8ad1eba8823fef925486f205df0b718a88e
ASP1234 <nimish@nimishs.in>
2016-06-28 09:59:47 +0000

    Minor Fix

--- data_structures_correct/CLKINV_f.js
+++ data_structures_correct/CLKINV_f.js
@@ -1,22 +1,22 @@
-function CLKINV_f () {
+function CLKINV_f() {
 
     CLKINV_f.prototype.define = function CLKINV_f() {
-		this.prt = 1;
+        this.prt = 1;
 
-		var model = scicos_model();
-		model.sim = new ScilabString(["input"]);
-		model.evtout = new ScilabDouble([1]);
-		model.ipar=new ScilabDouble([this.prt]);
-		model.blocktype = new ScilabString(["d"]);
-		model.firing = new ScilabDouble([-1]);
-		model.dep_ut = new ScilabBoolean([false,false]);
+        var model = scicos_model();
+        model.sim = new ScilabString(["input"]);
+        model.evtout = new ScilabDouble([1]);
+        model.ipar = new ScilabDouble([this.prt]);
+        model.blocktype = new ScilabString(["d"]);
+        model.firing = new ScilabDouble([-1]);
+        model.dep_ut = new ScilabBoolean([false, false]);
 
-		var exprs = new ScilabString([this.prt]);
-		var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"CLKINV_f\",sz(1),sz(2));"]);
-		this.x=new standard_define(new ScilabDouble([1,1]),model,exprs,gr_i);
-		return new EventInBlock(this.x);
+        var exprs = new ScilabString([this.prt]);
+        var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"CLKINV_f\",sz(1),sz(2));"]);
+        this.x = new standard_define(new ScilabDouble([1, 1]), model, exprs, gr_i);
+        return new EventInBlock(this.x);
     }
     CLKINV_f.prototype.details = function CLKINV_f() {
-		return this.x;
-	}
+        return this.x;
+    }
 }
