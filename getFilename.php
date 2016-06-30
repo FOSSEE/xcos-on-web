981429df495c0b30b4a0a3cfc4cdaf3d096b834b
jiteshjha <jiteshjha96@gmail.com>
2016-06-30 15:46:15 +0530

    Preloading-images-blocks-palettes-ds_files

---
+++ getFilename.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * @jiteshjha
+ * getFilename returns the names of all the files in a given folder
+ */
+
+// Store the filenames
+$filenameArray = [];
+
+// Get the folder to be operated
+$url = $_POST['url'];
+
+// Open the file directory and get the filehandle
+// Reference: http://php.net/manual/en/function.opendir.php
+$handle = opendir(dirname(realpath(__FILE__)).$url);
+
+// For each file in the folder, push the filename to filenameArray
+while($file = readdir($handle)){
+    if($file !== '.' && $file !== '..'){
+        array_push($filenameArray, "$url$file");
+    }
+}
+
+// Return the file name array in JSON format
+echo json_encode($filenameArray);
+?>
