84804a91a611aff7ad31741a8f4f7eae3daf4702
Pooja Soundalgekar <pooja27ms@gmail.com>
2016-07-03 01:57:45 +0530

    ds_MATEXPM

---
+++ data_structures_correct/MATEXPM.js
@@ -0,0 +1,34 @@
+function MATEXPM() {
+
+    MATEXPM.prototype.define = function MATEXPM() {
+        this.function_name = "mat_expm";
+        this.funtyp = 4;
+        var model = scicos_model();
+        model.sim = list(new ScilabString([this.function_name]), new ScilabDouble([this.funtyp]));
+        model.in = new ScilabDouble([-1]);
+        model.in2 = new ScilabDouble([-1]);
+        model.intyp = new ScilabDouble([1]);
+        model.out = new ScilabDouble([-1]);
+        model.out2 = new ScilabDouble([-1]);
+        model.outtyp = new ScilabDouble([1]);
+        model.evtin = new ScilabDouble();
+        model.evtout = new ScilabDouble();
+        model.state = new ScilabDouble();
+        model.dstate = new ScilabDouble();
+        model.rpar = new ScilabDouble();
+        model.ipar = new ScilabDouble();
+        model.blocktype = new ScilabString(["c"]);
+        model.firing = new ScilabDouble();
+        model.dep_ut = new ScilabBoolean([true, false]);
+
+        var label = new ScilabString([sci2exp(1)]);
+
+        var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"MATEXPM\",sz(1),sz(2));"]);
+        this.x = new standard_define(new ScilabDouble([2, 2]), model, label, gr_i);
+        return new BasicBlock(this.x);
+    }
+    MATEXPM.prototype.details = function MATEXPM() {
+        return this.x;
+    }
+}
+
