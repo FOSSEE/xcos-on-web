06dd8e39180637b23d58422a5cdfd57ad8a9f49b
Pooja Soundalgekar <pooja27ms@gmail.com>
2016-07-04 11:28:58 +0530

    ds_MATSUM modified

--- data_structures_correct/MATSUM.js
+++ data_structures_correct/MATSUM.js
@@ -32,11 +32,4 @@ function MATSUM() {
         return this.x;
     }
 }
-// This is just a sample script. Paste your real code (javascript or HTML) here.
-
-if ('this_is' == /an_example/) {
-    of_beautifier();
-} else {
-    var a = b ? (c % d) : e[f];
-}
 
