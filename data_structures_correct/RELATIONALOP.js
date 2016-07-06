e2a5adbbfe6ae9b1f5bacb8ff1f00adb49a01f8c
Pooja Soundalgekar <pooja27ms@gmail.com>
2016-07-06 13:13:14 +0530

    ds_RELATIONALOP

---
+++ data_structures_correct/RELATIONALOP.js
@@ -0,0 +1,26 @@
+function RELATIONALOP() {
+
+    RELATIONALOP.prototype.define = function RELATIONALOP() {
+        this.ipar = 2;
+        this.label = "&lt";
+
+        var model = scicos_model();
+        model.sim = list(new ScilabString(["relationalop"]), new ScilabDouble([4]));
+        model.in = new ScilabDouble([1], [1]);
+        model.out = new ScilabDouble([1]);
+        model.ipar = new ScilabDouble([this.ipar]);
+        model.blocktype = new ScilabString(["c"]);
+        model.dep_ut = new ScilabBoolean([true, false]);
+
+        var exprs = new ScilabString([this.ipar], [0]);
+
+        var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"RELATIONALOP\",sz(1),sz(2));"]);
+        this.x = new standard_define(new ScilabDouble([2, 2]), model, exprs, gr_i);
+        this.x.graphics.style = new ScilabString(["fontSize=13;fontStyle=1;displayedLabel=" + label]);
+        return new BasicBlock(this.x);
+    }
+    RELATIONALOP.prototype.details = function RELATIONALOP() {
+        return this.x;
+    }
+}
+
