b65f15a8172ce1492815df1472fb1566bdbecc26
Pooja Soundalgekar <pooja27ms@gmail.com>
2016-07-06 15:09:52 +0530

    ds_SINBLK_f

---
+++ data_structures_correct/SINBLK_f.js
@@ -0,0 +1,23 @@
+function SINBLK_f() {
+
+    SINBLK_f.prototype.define = function SINBLK_f() {
+        var model = scicos_model();
+        model.sim = new ScilabString(["sinblk"]);
+        model.in = new ScilabDouble([-1]);
+        model.out = new ScilabDouble([-1]);
+        model.blocktype = new ScilabString(["c"]);
+        model.dep_ut = new ScilabBoolean([true, false]);
+
+        var exprs = new ScilabString([" "]);
+
+        var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"SINBLK_f\",sz(1),sz(2));"]);
+        this.x = new standard_define(new ScilabDouble([2, 2]), model, exprs, gr_i);
+        return new BasicBlock(this.x);
+    }
+
+    SINBLK_f.prototype.details = function SINBLK_f() {
+        return this.x;
+
+    }
+}
+
