759f1602d493b6be76a9ff23d0e5740c11481a60
ASP1234 <nimish@nimishs.in>
2016-07-09 09:14:58 +0000

    Fix MATZREIM Block

--- data_structures_correct/MATZREIM.js
+++ data_structures_correct/MATZREIM.js
@@ -8,7 +8,7 @@ function MATZREIM() {
 
         this.funtyp = new ScilabDouble([4]);
         model.sim = list(this.function_name, this.funtyp);
-        model.in1 = new ScilabDouble([-1]);
+        model.in = new ScilabDouble([-1]);
         model.in2 = new ScilabDouble([-2]);
         model.intyp = new ScilabDouble([2]);
         model.out = new ScilabDouble(-1, -1);
