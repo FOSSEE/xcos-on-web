73e59c5466c8b4bd216044b2c9b509fec8bffd0d
Adhitya Kamakshidasan <adhitya07@gmail.com>
2016-07-11 21:44:24 +0530

    Fixed style bug for 'Explicit' Blocks

--- data_structures_correct/IN_f.js
+++ data_structures_correct/IN_f.js
@@ -15,7 +15,7 @@ function IN_f () {
 		var exprs = new ScilabString([sci2exp(this.prt)]);
 	
 		var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"IN_f\",sz(1),sz(2));"]);
-		var block=new standard_define(new ScilabDouble([1,1]),model,exprs,gr_i);
+		var block = new standard_define(new ScilabDouble([1,1]),model,exprs,gr_i);
 		block.graphics.style = new ScilabString(["IN_f"]); // changed
 		block.graphics.out_implicit = new ScilabString(["E"]);
 		block.graphics.out_style = new ScilabString(["ExplicitOutputPort;align=right;verticalAlign=middle;spacing=10.0;rotation=0"]);
@@ -31,14 +31,14 @@ function IN_f () {
 		model.out = new ScilabDouble([-1]);
 		model.out2 = new ScilabDouble([-2]);
 		model.outtyp = new ScilabDouble([-1]);
-		model.ipar=new ScilabDouble([this.prt]);
+		model.ipar = new ScilabDouble([this.prt]);
 		model.blocktype = new ScilabString(["c"]);
 		model.dep_ut = new ScilabBoolean([false,false]);
 	
 		var exprs = new ScilabString([sci2exp(this.prt)]);
 	
 		var gr_i = new ScilabString(["xstringb(orig(1),orig(2),\"IN_f\",sz(1),sz(2));"]);
-		this.x=new standard_define(new ScilabDouble([1,1]),model,exprs,gr_i);
+		this.x = new standard_define(new ScilabDouble([1,1]),model,exprs,gr_i);
 		return new ExplicitInBlock(this.x);
 	}
 	
